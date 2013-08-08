029c5b384d6c26ff749c47013a2b1388a210ed2a
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2013-08-08 13:29:31 -0300

    added some basic statistics

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def index
+    @accounts_cnt = Account.count
+    @users_cnt = User.count
+    @clients_cnt = Client.count
+    @appointments_cnt = Appointment.count
   end
 end
